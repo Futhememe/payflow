e6133efd4b10d270b9e9a9fb2e885360cc83c058
Gustavo <kyoro_souza@hotmail.com>
2021-09-22 20:14:46 -0300

    🎨 enable primary button style

--- lib/shared/widgets/set_label_buttons/set_label_buttons.dart
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vertical.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 
@@ -10,12 +11,17 @@ class SetLabelButtons extends StatelessWidget {
   final String secondLabel;
   final VoidCallback secondOnTap;
 
+  final bool enablePrimary;
+
+
   const SetLabelButtons({ 
     Key? key, 
     required this.primaryLabel, 
     required this.primaryOnTap, 
     required this.secondLabel, 
-    required this.secondOnTap 
+    required this.secondOnTap,
+    this.enablePrimary = false,
+
   }) : super(key: key);
 
   @override
@@ -24,7 +30,11 @@ class SetLabelButtons extends StatelessWidget {
       height: 56,
       child: Row(
         children: [
-          Expanded(child: LabelButton(label: primaryLabel, onPressed: primaryOnTap)),
+          Expanded(child: LabelButton(
+            label: primaryLabel, 
+            onPressed: primaryOnTap,
+            style: enablePrimary ? TextStyles.buttonPrimary : null,
+          )),
           DividerVertical(),
           Expanded(child: LabelButton(label: secondLabel, onPressed: secondOnTap)),
         ],
