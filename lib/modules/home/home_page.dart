c7be7afcb771503ed772bceb723f60e297ea7709
Gustavo <kyoro_souza@hotmail.com>
2021-09-21 21:43:03 -0300

    ✨ add new screen on app

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -79,7 +79,7 @@ class _HomePageState extends State<HomePage> {
               ),
             GestureDetector(
               onTap: (){
-                print('Cliclou');
+                Navigator.pushNamed(context, '/barcode_scanner');
               },
               child: Container(
                 height: 56,
